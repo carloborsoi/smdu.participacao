4121b874cc0a8a8b2769f6f058197541f3aea9e2
Davi Lima <davilima6@gmail.com>
2016-05-23 18:22:39 -0300

    Usa campo de texto rico com editor TinyMCE como widget

--- src/smdu/participacao/interfaces.py
+++ src/smdu/participacao/interfaces.py
@@ -5,13 +5,19 @@ from smdu.participacao import _
 from zope import schema
 from zope.interface import Interface
 from zope.publisher.interfaces.browser import IDefaultBrowserLayer
+# from plone.autoform import directives as aform
+# from plone.supermodel import model
+# from plone.app.z3cform.wysiwyg import WysiwygFieldWidget
+from plone.app.textfield import RichText
 
 
 class ISmduParticipacaoLayer(IDefaultBrowserLayer):
     """Marker interface that defines a browser layer."""
 
 
+# class IMinuta(model.Schema):
 class IMinuta(Interface):
+    """Dexterity content type model: fields and widgets."""
 
     title = schema.TextLine(
         title=_(u"Title"),
@@ -23,7 +29,9 @@ class IMinuta(Interface):
         required=False,
     )
 
-    text = schema.Text(
+    # aform.widget('text', WysiwygFieldWidget)
+    # text = schema.Text(
+    text = RichText(
         title=_(u"Rich Text"),
         required=False
     )
