3ae0971636b8e6db0f6f42def06e0f1255f106b1
Davi Lima <davilima6@gmail.com>
2016-05-30 18:39:20 -0300

    Registra componentes do indexador, da permissao e do assinante do evento de workflow para tipo de conteudo Minuta

---
+++ src/smdu/participacao/content/__init__.py
@@ -0,0 +1,3 @@
+# -*- coding: utf-8 -*-
+
+AvaliarMinuta = 'smdu.participacao: Avaliar minuta'
---
+++ src/smdu/participacao/content/minuta.py
@@ -0,0 +1,9 @@
+from plone.dexterity.browser import edit
+
+# class EditForm(edit.DefaultEditForm):
+#     """
+#     """
+    # def update(self):
+    #     super(EditForm, self).update()
+    #     self.fields['IRichText.text'].field.title = u'Corpo de texto da Minuta'
+    #     # import pdb; pdb.set_trace()
