e46daed3eee3eefdba0f8911ce6418ac5db373dd
Davi Lima <davilima6@gmail.com>
2016-06-01 14:51:59 -0300

    Atualiza indexador de votos positivos para somar o resultado de todos os paragrafos

--- src/smdu/participacao/indexers.py
+++ src/smdu/participacao/indexers.py
@@ -1,8 +1,16 @@
 from plone.indexer import indexer
 from plone.app.contenttypes.indexers import SearchableText as SearchableText_
 from plone.app.contenttypes.indexers import _unicode_save_string_concat
+from smdu.participacao.browser import rate
 from smdu.participacao.interfaces import IMinuta
+from cioppino.twothumbs.interfaces import ILoveThumbsDontYou
+
 
 @indexer(IMinuta)
 def SearchableText(obj):
     return _unicode_save_string_concat(SearchableText_(obj))
+
+
+@indexer(ILoveThumbsDontYou)
+def positive_ratings(obj):
+    return rate.get_total_avaliacoes_positivas(obj)
