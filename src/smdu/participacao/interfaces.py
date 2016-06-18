c52e0e4770a0dbd796c9ea7ad49acee90a308c64
Carlo Borsoi Moura <carlo.moura@alliare.com.br>
2016-06-18 15:09:51 -0300

    Criação do content type Proposta

--- src/smdu/participacao/interfaces.py
+++ src/smdu/participacao/interfaces.py
@@ -4,7 +4,9 @@
 from zope import schema
 from zope.publisher.interfaces.browser import IDefaultBrowserLayer
 from plone.supermodel import model
-
+from plone.namedfile.field import NamedImage
+from plone.formwidget.geolocation.field import GeolocationField
+from plone.directives import form
 
 class ISmduParticipacaoLayer(IDefaultBrowserLayer):
     """Marker interface that defines a browser layer."""
@@ -35,3 +37,47 @@ class IMinuta(model.Schema):
 
 class IConsulta_Publica(model.Schema):
     """Modelagem do tipo de conteudo Dexterity: campos e widgets."""
+
+class IProposta(model.Schema):
+    """Modelagem do tipo de conteudo Dexterity: campos e widgets."""
+
+
+    justificativa = schema.Text(
+        title=u'Justifciativa da proposta',
+    )
+
+    endereco = schema.TextLine(
+        title=u'Endereço',
+        required=False,
+        max_length = 500
+    )
+
+    bairro = schema.TextLine(
+            title=(u'Bairro'),
+            description=(u'Informe o bairro.'),
+            max_length=50,
+            required=False)
+
+    cep = schema.TextLine(
+        title=(u'CEP'),
+        description=(u'Informe o CEP (apenas números)'),
+        max_length=8,
+        min_length=8,
+        required=False)
+
+    imagem = NamedImage(
+        title=(u"Caso possua, insira uma imagem relativo à proposta"),
+        required=False,
+    )
+
+    localizazao = GeolocationField(
+        title=(u"Insira a localização no mapa relativa à proposta"),
+        required=False,
+    )
+
+@form.validator(field=IProposta['cep'])
+def validateCep(value):
+    if value:
+        value = ''.join([c for c in value if c.isdigit()])
+        if len(value) != 8:
+            raise interface.Invalid((u'CEP inválido.'))
