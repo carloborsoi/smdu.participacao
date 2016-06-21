2248713255a1ec19b4e17c18df41326260381409
Davi Lima <davilima6@gmail.com>
2016-06-20 21:56:26 -0300

    Usa constraint, evitando uso de formlib no validador

--- src/smdu/participacao/interfaces.py
+++ src/smdu/participacao/interfaces.py
@@ -2,23 +2,28 @@
 """Módulo onde vivem todas as interfaces, eventos e exceções."""
 
 from zope import schema
+from zope.interface import Invalid
 from zope.publisher.interfaces.browser import IDefaultBrowserLayer
 from plone.supermodel import model
 from plone.namedfile.field import NamedImage
 from plone.formwidget.geolocation.field import GeolocationField
-from plone.directives import form
+
+
 
 class ISmduParticipacaoLayer(IDefaultBrowserLayer):
-    """Marker interface that defines a browser layer."""
+    """Interface de marcação que define um browser layer.
+    """
 
 
 class IMinuta(model.Schema):
-    """Modelagem do tipo de conteudo Dexterity: campos e widgets."""
+    """Modelagem do tipo de conteúdo Minuta: campos e widgets.
+    """
 
-    model.fieldset('extra',
-                   label=u"Comentários inline",
-                   fields=['disableAnnotator', 'readOnlyAnnotator']
-                   )
+    model.fieldset(
+        'extra',
+        label=u"Comentários inline",
+        fields=['disableAnnotator', 'readOnlyAnnotator']
+    )
 
     disableAnnotator = schema.Bool(
         title=u'Desativar avaliação',
@@ -36,48 +41,52 @@ class IMinuta(model.Schema):
 
 
 class IConsulta_Publica(model.Schema):
-    """Modelagem do tipo de conteudo Dexterity: campos e widgets."""
+    """Modelagem do tipo de conteúdo Consulta Pública: container de Propostas.
+    """
+
+
+def validateCep(value):
+    if value:
+        value = ''.join([c for c in value if c.isdigit()])
+        if len(value) != 8:
+            raise Invalid((u'CEP inválido.'))
+    return True
+
 
 class IProposta(model.Schema):
-    """Modelagem do tipo de conteudo Dexterity: campos e widgets."""
-
+    """Modelagem do tipo de conteúdo Proposta: campos e widgets."""
 
     justificativa = schema.Text(
-        title=u'Justifciativa da proposta',
+        title=u'Justificativa da proposta',
     )
 
     endereco = schema.TextLine(
         title=u'Endereço',
         required=False,
-        max_length = 500
+        max_length=500
     )
 
     bairro = schema.TextLine(
-            title=(u'Bairro'),
-            description=(u'Informe o bairro.'),
-            max_length=50,
-            required=False)
+        title=(u'Bairro'),
+        description=(u'Informe o bairro.'),
+        max_length=50,
+        required=False)
 
     cep = schema.TextLine(
         title=(u'CEP'),
         description=(u'Informe o CEP (apenas números)'),
         max_length=8,
         min_length=8,
-        required=False)
+        required=False,
+        constraint=validateCep
+    )
 
     imagem = NamedImage(
-        title=(u"Caso possua, insira uma imagem relativo à proposta"),
+        title=(u"Caso possua, insira uma imagem relativa à proposta"),
         required=False,
     )
 
-    localizazao = GeolocationField(
+    localizacao = GeolocationField(
         title=(u"Insira a localização no mapa relativa à proposta"),
         required=False,
     )
-
-@form.validator(field=IProposta['cep'])
-def validateCep(value):
-    if value:
-        value = ''.join([c for c in value if c.isdigit()])
-        if len(value) != 8:
-            raise interface.Invalid((u'CEP inválido.'))
