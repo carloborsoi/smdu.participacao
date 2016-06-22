7b7dec9866a5d5b7b0b1d81983ed338d7c3cc0ba
Carlo Borsoi Moura <carlo.moura@alliare.com.br>
2016-06-22 18:09:57 -0300

    Corrige o problema do envio da autenticação CSRF para formaluário de Proposta

--- src/smdu/participacao/browser/static/js/proposta.js
+++ src/smdu/participacao/browser/static/js/proposta.js
@@ -7,8 +7,14 @@
       // Botao de Apoiar Proposta
       $(".apoiar-form").on("submit", function(e) {
           e.preventDefault();
-          var url = $(this).attr("action");
-          $.post(url).done(function(data) {
+          var $this = $(this);
+          var auth_key = $this.find('input[name="_authenticator"]').val();
+          var params = {
+              'ajax': 1,
+              '_authenticator': auth_key
+          };
+          var url = $this.attr("action");
+          $.post(url, params).done(function(data) {
               alert(data);
           });
       });
