1e1dc5b9d32572d00e69fbee92d2054dade40d80
Carlo Borsoi Moura <carlo.moura@alliare.com.br>
2016-11-09 12:45:08 -0200

    Correção do campo Mapa

--- src/smdu/participacao/behaviors/mapa.py
+++ src/smdu/participacao/behaviors/mapa.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from plone.supermodel import  src/smdu/participacao/profiles/default/types/Proposta.xml, directives
+from plone.supermodel import model, directives
 from plone.autoform.interfaces import IFormFieldProvider
 from zope import schema
 from zope.interface import alsoProvides
